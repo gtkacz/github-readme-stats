d4e3565d4fb4d3d2c025b98dbf76ceabac0c3424
Gabriel Tkacz <55806524+gtkacz@users.noreply.github.com>
2021-03-04 11:17:13 -0300

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -9,7 +9,7 @@ const themes = {
     title_color: "2f80ed",
     icon_color: "4c71f2",
     text_color: "333",
-    bg_color: "0D1117",
+    bg_color: "0d1117",
   },
   default_repocard: {
     title_color: "2f80ed",
